8602ada1f00819363a8aaefb2a61b3b36c1ccd60
SaqinOO7 <peter895832@gmail.com>
2024-09-05 19:46:48 +0530

    Socket url updated

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://tic-tac-toe-game-prod.onrender.com", {
                 query: {
                     userId: authUser.id,        //this authUser.id  not ._id
                 }
